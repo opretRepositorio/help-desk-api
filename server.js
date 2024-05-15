93945be28aee550f41e918098900dc175d50ec07
jose lopez <joseopret@gmail.com>
2024-05-15 09:42:56 -0400

    modified:   server.js

--- server.js
+++ server.js
@@ -9,12 +9,6 @@ app.use(cors({
   origin: "*",
 }));
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
-
 // Middleware
 app.use(express.json()); // parse json bodies in the request object
 
